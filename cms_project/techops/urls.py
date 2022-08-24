20f5649c944d2ed6ff28a910d0bde66959b333d7
Omar Mahmood <omarmahmood005@gmail.com>
2022-08-24 21:31:49 +0530

    removed branch varun for re-adding

--- cms_project/techops/urls.py
+++ cms_project/techops/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('techops_login_page', views.techops_login),
     path('techops_signup_page', views.techops_signup),
     path('techops_dashboard', views.techops_dashboard),
-    path('testing', views.techops_dashboard),
-    path('random', views.techops_dashboard),
 ]
